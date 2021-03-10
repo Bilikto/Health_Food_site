36b2f4c99717c5bd791a49c9abe2ac8f9a0571ba
Bilikto <dashievbilikto@gmail.com>
2021-03-10 14:29:47 -0500

    timer is created

--- js/script.js
+++ js/script.js
@@ -83,6 +83,57 @@ window.addEventListener("DOMContentLoaded", () => {
   const timerId = setTimeout(openModal, 30000);
 
 
+  //Timer
+  const deadLine = "2022.03.10";
+
+  const getTimeRemaining = (endTime) => {
+    const total = Date.parse(endTime) - Date.parse(new Date()),
+          days = Math.floor(total / (1000*60*60*24)),
+          hours = Math.floor(total / (1000*60*60) % 24),
+          minutes = Math.floor((total / 1000 / 60) % 60),
+          seconds = Math.floor((total / 1000) % 60);
+          
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  };
+
+  const getZero = (num) => {
+    if (num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  };
+
+  const setTimer = (endTime) => {
+    const timer = document.querySelector('.timer'),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timerId = setInterval(updateTimer);
+    
+      function updateTimer() {
+        const t = getTimeRemaining(endTime);
+        
+          days.textContent = getZero(t.days);
+          hours.textContent = getZero(t.hours);
+          minutes.textContent = getZero(t.minutes);
+          seconds.textContent = getZero(t.seconds);
+
+        if (t === 0) {
+          clearInterval(timerId);
+        }
+      }
+      updateTimer();
+  };
+
+  setTimer(deadLine);
 
 
 
