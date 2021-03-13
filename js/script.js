0c5c25ad3f610f0c37b72d3855d1b152e0ccb526
Bilikto <dashievbilikto@gmail.com>
2021-03-13 12:11:19 -0500

    Calorie calculator is implemented

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener('scroll', showModalByScroll);
 
-  const timerId = setTimeout(openModal, 30000);
+  const timerId = setTimeout(openModal, 50000);
 
 
   //Timer
@@ -138,17 +138,17 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //Slider
   const slider = document.querySelector('.offer__slider'),
-        prevArrow = slider.querySelector('.offer__slider-prev'),
-        nextArrow = slider.querySelector('.offer__slider-next'),
-        currentCounter = slider.querySelector('#current'),
-        totalCounter = slider.querySelector('#total'),
-        slidesWrapper = slider.querySelector('.offer__slider-wrapper'),
-        slidesField = slider.querySelector('.offer__slider-inner'),
-        slides = slider.querySelectorAll('.offer__slide'),
-        width = window.getComputedStyle(slidesWrapper).width;
+    prevArrow = slider.querySelector('.offer__slider-prev'),
+    nextArrow = slider.querySelector('.offer__slider-next'),
+    currentCounter = slider.querySelector('#current'),
+    totalCounter = slider.querySelector('#total'),
+    slidesWrapper = slider.querySelector('.offer__slider-wrapper'),
+    slidesField = slider.querySelector('.offer__slider-inner'),
+    slides = slider.querySelectorAll('.offer__slide'),
+    width = window.getComputedStyle(slidesWrapper).width;
 
   let slidesIndex = 1,
-      offSet = 0;
+    offSet = 0;
 
   slidesField.style.width = 100 * slides.length + '%';
   slidesField.style.display = 'flex';
@@ -163,7 +163,7 @@ window.addEventListener("DOMContentLoaded", () => {
   slider.style.position = 'relative';
 
   const indicator = document.createElement('ol'),
-        dots = [];
+    dots = [];
   indicator.classList.add('carousel-indicators');
   slider.append(indicator);
 
@@ -175,7 +175,7 @@ window.addEventListener("DOMContentLoaded", () => {
     if (i == 0) {
       dot.style.opacity = 1;
     }
-    
+
     indicator.append(dot);
     dots.push(dot);
   }
@@ -253,7 +253,113 @@ window.addEventListener("DOMContentLoaded", () => {
   changeOpacity();
 
 
+  //Calorie Calculator
+  const result = document.querySelector('.calculating__result span');
+  let sex, height, weight, age, ratio;
 
+  if (localStorage.getItem('sex')) {
+    sex = localStorage.getItem('sex');
+  } else {
+    sex = 'female';
+    localStorage.setItem('sex', 'female');
+  }
+
+  if (localStorage.getItem('ratio')) {
+    ratio = localStorage.getItem('ratio');
+  } else {
+    ratio = 1.375;
+    localStorage.setItem('ratio', 1.375);
+  }
+
+  const initLocalSettings = (selector, activeClass) => {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach(item => {
+      item.classList.remove(activeClass);
+
+      if (item.getAttribute('id') == [sex]) {
+        item.classList.add(activeClass);
+      }
+
+      if (item.getAttribute('data-ratio') == [ratio]) {
+        item.classList.add(activeClass);
+      }
+    });
+  };
+  
+
+  const calcResult = () => {
+    if (!sex || !height || !weight || !age || !ratio) {
+      result.textContent = "____";
+      return;
+    }
+
+    if (sex === "female") {
+      result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+    } else {
+      result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+    }
+  };
+
+
+  const getStaticInfo = (selector, activeClass) => {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach(item => {
+      item.addEventListener('click', (e) => {
+        if (e.target.getAttribute('data-ratio')) {
+          ratio = +e.target.getAttribute('data-ratio');
+          localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
+        } else {
+          sex = e.target.getAttribute('id');
+          localStorage.setItem('sex', e.target.getAttribute('id'));
+        }
+
+        elements.forEach(elem => {
+          elem.classList.remove(activeClass);
+        });
+        item.classList.add(activeClass);
+
+        calcResult();
+      });
+    });
+  };
+
+
+  const getDynamicInfo = (selector) => {
+    const input = document.querySelector(selector);
+
+    input.addEventListener('input', () => {
+      if (input.value.match(/\D/ig)) {
+        input.style.border = '1px solid red';
+      } else {
+        input.style.border = 'none';
+      }
+
+      switch (input.getAttribute('id')) {
+        case 'height':
+          height = +input.value;
+          break;
+        case 'weight':
+          weight = +input.value;
+          break;
+        case 'age':
+          age = +input.value;
+          break;
+      }
+
+      calcResult();
+    });
+  };
+
+  initLocalSettings('#gender div', 'calculating__choose-item_active');
+  initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+  calcResult();
+  getStaticInfo('#gender div', 'calculating__choose-item_active');
+  getStaticInfo('.calculating__choose_big div', 'calculating__choose-item_active');
+  getDynamicInfo('#height');
+  getDynamicInfo('#weight');
+  getDynamicInfo('#age');
 
 
 
