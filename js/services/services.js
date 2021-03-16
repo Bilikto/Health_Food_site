84c1e12224abe05a32473669bbb64b974df789cb
Bilikto <dashievbilikto@gmail.com>
2021-03-15 23:52:25 -0400

    added services for requests (fetch API)

---
+++ js/services/services.js
@@ -0,0 +1,31 @@
+const getResource = async (url) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Could not fetch ${url}, status ${response.status}`);
+  }
+
+  return await response.json();
+};
+
+
+const postData = async (url, data) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    body: data
+  });
+
+  if (!response.ok) {
+    throw new Error(`Could not fetch ${url}, status ${response.status}`)
+  }
+
+  return await response.json();
+};
+
+export {getResource, postData};
+
+
+
